313615228364481cdaa5eb75ae2cfe5809d3b5ca
Melanie Tran <melaniexx02@gmail.com>
2020-05-22 11:02:56 -0600

    Add MVC for user story 3 fill in form to add actor

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,4 +2,15 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  def create
+    @movie = Movie.find(params[:id])
+    @movie.actors.create!(actor_movie_params)
+    redirect_to "/movies/#{@movie.id}"
+  end
+
+  private
+  def actor_movie_params
+    params.permit(:name)
+  end
 end
